2155d1d596f081873902a97a90e9c9d6bed2d521
Aravind Madipozu <aravindias1@gmail.com>
2025-09-09 16:32:15 +0530

    helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,1 +1,8 @@
-
+@@ -0,0 +1,7 @@
+class helloworld {
+public static void main(String[] args) {
+System.out.println("Try programiz.pro");
+System.out.println("Hello World");
+System.out.println("Welcome to Java Programming");
+}
+}
